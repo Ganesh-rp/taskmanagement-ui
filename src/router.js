508158e700ea95ba7ab10e0d5c2701741190f3fa
ganeshrp <ganeshwaytoit@gmail.com>
2021-06-30 03:46:08 +0530

    router changes

--- src/router.js
+++ src/router.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Router, Route, Switch, useHistory } from 'react-router-dom';
 import { Layout } from 'antd';
 import CreateProject from './pages/createproject';
 import Login from './pages/login';
-import NotFound from './pages/notfound';
 import Project from './pages/projects';
 import SignUp from './pages/signup';
 import CommonHeader from './components/Header';
 
 
-const { Header, Footer, Content } = Layout;
+const { Content } = Layout;
 
 
 const Routes = () => {
+    const history = useHistory();
+
     return (
-        <Router history={useHistory()}>
+        <Router history={history}>
             <Switch>
                 <Route exact path="/" component={Login} />
-                <Route  path="/login" component={Login} />
-                <Route  path="/signup" component={SignUp} />
+                <Route path="/login" component={Login} />
+                <Route path="/signup" component={SignUp} />
                 <Layout>
-                    <CommonHeader history={useHistory()} />
+                    <CommonHeader history={history} />
                     <Content>
-                        <Route  path="/project" component={Project} />
-                        <Route  path="/createproject" component={CreateProject} />
+                        <Route path="/project" component={ Project} />
+                        <Route path="/createproject" component={CreateProject} />
                     </Content>
                 </Layout>
-                <Route  path="*" component={NotFound} />
             </Switch>
         </Router>
     )
